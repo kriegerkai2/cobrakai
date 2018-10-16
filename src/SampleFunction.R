e0622f15c7586b4121a6acc846ad3000f997f998
kriegerkai2 <44210880+kriegerkai2@users.noreply.github.com>
2018-10-16 14:32:48 -0600

    Update src/SampleFunction.R
    created an r script

---
+++ src/SampleFunction.R
@@ -0,0 +1,10 @@
+#test script for GitHub
+myfunction <- function(x,y){
+  if(x>y){
+    print("x is greater than y")
+  }else{
+    print("y is greater than or equal to x")
+  }
+}
+
+z<-myfunction(x=10,y=5)
